c1e70f502e1a52fd25082e19d90b247c4c3de5c6
sobirinjamo33 <sobirinjamo33@gmail.com>
2025-01-24 09:46:04 +0700

    Update Layout

--- resources/views/dashboards/index.blade.php
+++ resources/views/dashboards/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="row">
-        <div class="col-xxl-5">
+        <div class="col-xxl-12">
             <div class="row">
                 <div class="col-12">
                     <div class="alert alert-primary text-truncate mb-3" role="alert">
@@ -10,7 +10,7 @@
                     </div>
                 </div>
 
-                <div class="col-md-6">
+                <div class="col-md-3">
                     <div class="card overflow-hidden">
                         <div class="card-body">
                             <div class="row">
@@ -41,7 +41,7 @@
                         </div> 
                     </div> <!-- end card -->
                 </div> <!-- end col -->
-                <div class="col-md-6">
+                <div class="col-md-3">
                     <div class="card overflow-hidden">
                         <div class="card-body">
                             <div class="row">
@@ -70,7 +70,7 @@
                         </div>  <!-- end card body -->
                     </div> <!-- end card -->
                 </div> <!-- end col -->
-                <div class="col-md-6">
+                <div class="col-md-3">
                     <div class="card overflow-hidden">
                         <div class="card-body">
                             <div class="row">
@@ -100,7 +100,7 @@
                         </div> <!-- end card body -->
                     </div> <!-- end card -->
                 </div> <!-- end col -->
-                <div class="col-md-6">
+                <div class="col-md-3">
                     <div class="card overflow-hidden">
                         <div class="card-body">
                             <div class="row">
@@ -129,7 +129,7 @@
             </div> <!-- end row -->
         </div> <!-- end col -->
 
-        <div class="col-xxl-7">
+        <div class="col-xxl-12">
             <div class="card">
                 <div class="card-body">
                     <div class="d-flex justify-content-between align-items-center">
